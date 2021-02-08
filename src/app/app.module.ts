ba419a4399c268cef707e7a5e388e3b613ba9fb1
nottiffchan <nottiffchan@gmail.com>
2021-02-09 00:02:08 +0800

    Adjust layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { MenuPageComponent } from './menu-page/menu-page.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
